93e410976052640848351f0611fd756392597b7f
Spasimir Pavlov <spasimir.pavlov@gmail.com>
2024-09-04 12:03:43 +0300

    Updated project setup

--- frontend/config.ts
+++ frontend/config.ts
@@ -8,7 +8,7 @@ interface FrontendConfig {
 const FRONTEND_CONFIG: FrontendConfig = {
   publicBaseUrl: 'http://furnituro.com',
   oauth: {
-    googleAppId: '757962824139-o3erncpmc9rqtshvq440ainjsfdu52ft.apps.googleusercontent.com'
+    googleAppId: '213962799516-54mk54eoqi2t7b9mnnerhd5nqmm6taqj.apps.googleusercontent.com'
   }
 };
 
